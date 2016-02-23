2c77a39b836072b4559bc134c802986f3ddaaf36
Jordan Sexton <jordan@jordansexton.com>
2016-02-23 04:38:00 -0600

    removed crosswalk aar file; download in postinstall script

--- crosswalk.sh
+++ crosswalk.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
 ver="16.45.421.19"
+
 wget https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/${ver}/xwalk_core_library-${ver}.aar
 unzip -j xwalk_core_library-${ver}.aar classes.jar
 zip -d classes.jar javax\*
 zip -r xwalk_core_library-${ver}.aar classes.jar
 rm -f classes.jar
+mv xwalk_core_library-${ver}.aar libs/
