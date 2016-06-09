f34c9a5517aaf94116b6b235d8541785c77f0f86
Jordan Sexton <jordan@jordansexton.com>
2016-06-09 17:26:10 -0500

    update xWalk to 18.48.477.13

--- crosswalk.sh
+++ crosswalk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ver="17.46.448.10"
+ver="18.48.477.13"
 
 wget https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/${ver}/xwalk_core_library-${ver}.aar
 unzip -j xwalk_core_library-${ver}.aar classes.jar
